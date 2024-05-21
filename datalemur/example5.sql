9c1e19d66659f8549565cee3cc4045ea38d5ce07
ozkancondek <70811113+ozkancondek@users.noreply.github.com>
2024-05-21 17:04:05 +0200

    Create example5.sql

---
+++ datalemur/example5.sql
@@ -0,0 +1,5 @@
+--SQL Question 5: Can you provide an example of two entities that have a one-to-one relationship, and another example of a one-to-many relationship?
+
+--In database schema design, a one-to-one relationship between two entities means that each entity is linked to a single instance of the other. For example, the relationship between a car and a license plate is one-to-one because each car has only one license plate, and each license plate belongs to one car.
+
+--In contrast, a one-to-many relationship is when one entity can be associated with multiple instances of the other entity. An example of this is the relationship between a teacher and their classes - a teacher can teach multiple classes, but each class is only associated with one teacher
