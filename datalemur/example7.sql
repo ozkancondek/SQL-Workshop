71e9320e8f5b1145f8269381156e2d3621a82c33
ozkancondek <70811113+ozkancondek@users.noreply.github.com>
2024-05-23 09:43:17 +0200

    Create example7.sql

---
+++ datalemur/example7.sql
@@ -0,0 +1,7 @@
+/*
+SQL Question 7: What is database normalization?
+
+Normalization involves dividing a large table into smaller, more specific ones and establishing connections between them. 
+This helps to reduce redundancy, creating a database that is more adaptable, scalable, and easy to manage. Additionally, 
+normalization helps to maintain the integrity of the data by minimizing the risk of inconsistencies and anomalies.
+*/
