04a8771cbaf73a532012c261f5bf496bc60530f2
ozkancondek <70811113+ozkancondek@users.noreply.github.com>
2024-06-05 10:37:03 +0200

    Update ex9.sql

--- DL-Exercises/easy/ex9.sql
+++ DL-Exercises/easy/ex9.sql
@@ -1,32 +1,9 @@
---Dublicate Job Listing
-
-WITH job_count_cte AS (
-  SELECT 
-    company_id, 
-    title, 
-    description, 
-    COUNT(job_id) AS job_count
-  FROM job_listings
-  GROUP BY company_id, title, description
-)
-
-SELECT COUNT(DISTINCT company_id) AS duplicate_companies
-FROM job_count_cte
-WHERE job_count > 1;
-
-
-
-
-
-
-SELECT COUNT(DISTINCT company_id) AS duplicate_companies
-FROM (
-  SELECT 
-    company_id, 
-    title, 
-    description, 
-    COUNT(job_id) AS job_count
-  FROM job_listings
-  GROUP BY company_id, title, description
-) AS job_count_cte
-WHERE job_count > 1;
+--Cities With Completed Trades
+SELECT users.city,count(trades.order_id)  as cc
+FROM trades
+JOIN users ON users.user_id = trades.user_id 
+WHERE trades.status = 'Completed'
+GROUP BY users.city
+ORDER BY cc DESC
+LIMIT 3
+;
