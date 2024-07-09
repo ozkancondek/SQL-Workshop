4dbbb7596e4ef21a7bc681009ac82444720d4a1a
ozkancondek <70811113+ozkancondek@users.noreply.github.com>
2024-07-09 11:33:17 +0200

    Create ex9.sql

---
+++ DL-Exercises/medium/ex9.sql
@@ -0,0 +1,10 @@
+--Compressed Mode
+
+
+SELECT item_count AS mode
+FROM items_per_order
+WHERE order_occurrences = (
+  SELECT MAX(order_occurrences) 
+  FROM items_per_order
+)
+ORDER BY item_count;
