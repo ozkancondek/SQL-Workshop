3a7d7827cbd25a87fcae43be63ee370d58036f85
ozkancondek <70811113+ozkancondek@users.noreply.github.com>
2024-07-11 14:18:12 +0200

    Create ex10.sql

---
+++ DL-Exercises/medium/ex10.sql
@@ -0,0 +1,31 @@
+--International Call Percentage
+
+/* SELECT  
+  caller.country_id AS caller_country,
+  receiver.country_id AS receiver_country
+FROM phone_calls AS calls
+LEFT JOIN phone_info AS caller
+  ON calls.caller_id = caller.caller_id
+LEFT JOIN phone_info AS receiver
+  ON calls.receiver_id = receiver.caller_id;
+*/
+
+WITH international_calls AS (
+SELECT 
+  caller.caller_id, 
+  caller.country_id,
+  receiver.caller_id, 
+  receiver.country_id
+FROM phone_calls AS calls
+LEFT JOIN phone_info AS caller
+  ON calls.caller_id = caller.caller_id
+LEFT JOIN phone_info AS receiver
+  ON calls.receiver_id = receiver.caller_id
+WHERE caller.country_id <> receiver.country_id
+)
+
+SELECT 
+  ROUND(
+    100.0 * COUNT(*)
+  / (SELECT COUNT(*) FROM phone_calls),1) AS international_call_pct
+FROM international_calls;
