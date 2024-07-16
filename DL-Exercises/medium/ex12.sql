3bd9adbcb3d779f322c59f5034aa244fd444c190
ozkancondek <70811113+ozkancondek@users.noreply.github.com>
2024-07-16 16:34:15 +0200

    Create ex12.sql

---
+++ DL-Exercises/medium/ex12.sql
@@ -0,0 +1,13 @@
+--Patient Support Analysis 
+
+WITH uncategorised_callers AS (
+  SELECT COUNT(case_id) AS uncategorised_calls
+  FROM callers
+  WHERE call_category IS NULL
+    OR call_category = 'n/a'
+)
+
+SELECT 
+  ROUND(100.0 * uncategorised_calls 
+    / (SELECT COUNT(*) FROM callers), 1) AS uncategorised_call_pct
+FROM uncategorised_callers;
